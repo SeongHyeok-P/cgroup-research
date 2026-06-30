1b52174cf37ee93da905d71c96fb83c130aa4283
SeongHyeok-P <toggi61366@gmail.com>
2026-07-01 00:05:07 +0900

    add protected daemon workspace and scripts

---
+++ prototypes/clear_cgroup.sh
@@ -0,0 +1,11 @@
+sudo rmdir /sys/fs/cgroup/cgroup_research/System/Critical 2>/dev/null
+sudo rmdir /sys/fs/cgroup/cgroup_research/System/Protect 2>/dev/null
+sudo rmdir /sys/fs/cgroup/cgroup_research/System/Maintenance 2>/dev/null
+sudo rmdir /sys/fs/cgroup/cgroup_research/System/Service 2>/dev/null
+sudo rmdir /sys/fs/cgroup/cgroup_research/System 2>/dev/null
+
+sudo rmdir /sys/fs/cgroup/cgroup_research/User/Mid 2>/dev/null
+sudo rmdir /sys/fs/cgroup/cgroup_research/User/Low 2>/dev/null
+sudo rmdir /sys/fs/cgroup/cgroup_research/User/High 2>/dev/null
+sudo rmdir /sys/fs/cgroup/cgroup_research/User/Important 2>/dev/null
+sudo rmdir /sys/fs/cgroup/cgroup_research/User 2>/dev/null
